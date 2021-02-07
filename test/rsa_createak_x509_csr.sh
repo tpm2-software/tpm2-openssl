0e53b762f2f0ba1b8177b8b046fa803793a7596a
Petr Gotthard <petr.gotthard@advantech-bb.cz>
2021-02-07 22:49:14 +0100

    Modify X.509 tests to use config files

--- test/rsa_createak_x509_csr.sh
+++ test/rsa_createak_x509_csr.sh
@@ -10,8 +10,21 @@ tpm2_createak -C ek_rsa.ctx -G rsa -g sha256 -s rsassa -c ak_rsa.ctx
 # load the AK to persistent handle
 HANDLE=$(tpm2_evictcontrol --object-context=ak_rsa.ctx | cut -d ' ' -f 2 | head -n 1)
 
+cat > testcert.conf << EOF
+[ req ]
+default_bits        = 2048
+encrypt_key         = no
+prompt              = no
+
+distinguished_name  = req_dn
+
+[ req_dn ]
+countryName         = GB
+commonName          = Common Name
+EOF
+
 # create a private key and then generate a certificate request from it
-openssl req -provider tpm2 -new -subj "/C=GB/CN=foo" -key handle:${HANDLE} -out testcsr.pem
+openssl req -provider tpm2 -new -config testcert.conf -key handle:${HANDLE} -out testcsr.pem
 
 # display private key info
 openssl rsa -provider tpm2 -in handle:${HANDLE} -text -noout
@@ -22,4 +35,4 @@ openssl req -text -noout -verify -in testcsr.pem
 # release persistent handle
 tpm2_evictcontrol --object-context=${HANDLE}
 
-rm ek_rsa.ctx ak_rsa.ctx testcsr.pem
+rm ek_rsa.ctx ak_rsa.ctx testcert.conf testcsr.pem
--- test/rsa_genpkey_x509_cert.sh
+++ test/rsa_genpkey_x509_cert.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -eufx
 
+cat > testcert.conf << EOF
+[ req ]
+default_bits        = 2048
+default_keyfile     = testkey.pem
+encrypt_key         = no
+prompt              = no
+
+distinguished_name  = req_dn
+
+[ req_dn ]
+countryName         = GB
+commonName          = Common Name
+EOF
+
 # create a private key and then generate a self-signed certificate for it
-openssl req -provider tpm2 -x509 -sigopt pad-mode:pss -subj "/C=GB/CN=foo" -keyout testkey.pem -out testcert.pem
+openssl req -provider tpm2 -x509 -sigopt pad-mode:pss -config testcert.conf -out testcert.pem
 
 # display private key info
 openssl rsa -provider tpm2 -in testkey.pem -text -noout
@@ -13,4 +27,4 @@ openssl x509 -text -noout -in testcert.pem
 # verify the certificate
 openssl verify -verbose -CAfile testcert.pem testcert.pem
 
-rm testkey.pem testcert.pem
+rm testcert.conf testkey.pem testcert.pem
