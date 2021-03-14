1df492f5a2e5ae0a1475e3cef1cc8353d150afa7
Petr Gotthard <petr.gotthard@centrum.cz>
2021-03-14 19:25:03 +0100

    Add tests for store failures

---
+++ test/store_errors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -ufx
+
+function should_fail()
+{
+    WAS=$?
+    cat out
+    if [ $WAS -eq 0 ]; then exit 1; fi
+    grep -q "$1" out || exit 1
+}
+
+# unknown prefix
+openssl rsa -provider tpm2 -in bad:prefix -modulus -noout 2> out
+should_fail "Could not open file or uri"
+
+# unknown file
+openssl rsa -provider tpm2 -in file:unknown -modulus -noout 2> out
+should_fail "Could not open file or uri"
+
+# unknown TPM2 handle
+openssl rsa -provider tpm2 -in handle:0xBAD -modulus -noout 2> out
+should_fail "Could not read"
+
+# unknown TPM2 object
+openssl rsa -provider tpm2 -in object:unknown -modulus -noout 2> out
+should_fail "Could not open file or uri"
+
+# large wrong file
+dd if=/dev/zero of=largefile bs=1 count=1 seek=10240
+openssl rsa -provider tpm2 -in object:largefile -modulus -noout 2> out
+should_fail "Could not read"
+
+rm out largefile
